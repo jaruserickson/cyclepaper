64b59fa15857e1f7a6958a9c00b9cee8b63689f4
Jacob Erickson <jacob.erickson@mail.utoronto.ca>
2019-08-19 19:54:49 -0400

    update for source saving

--- src/App.js
+++ src/App.js
@@ -2,32 +2,37 @@ import React from 'react';
 import SubRedditCard from './SubRedditCard';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.defaultSubRedditSources = [
-      'wallpapers',
-      'wallpaper',
-      'multiwall'
-    ]
-  }
-
-  select = (subReddit) => {
-    
-  }
-  
-  render() {
-    return (
-      <div className="bg-near-black white vw-100 vh-100 flex flex-column items-center justify-center">
-        <span className="f2-ns tracked-tight helvetica pa3">cyclepaper</span>
-        {
-          this.defaultSubRedditSources.map((subReddit) => (
-            <SubRedditCard selectSubReddit={this.select}>{subReddit}</SubRedditCard>
-          ))
+    constructor(props) {
+        super(props)
+        this.state = {
+            sources: {
+                'wallpapers': false,
+                'wallpaper': false,
+                'multiwall': false
+            }
         }
-        {/* <button></button> */}
-      </div>
-    );
-  }
+    }
+
+    select = (subReddit) => {
+        const copySources = {...this.state.sources}
+        copySources[subReddit] = !this.state.sources[subReddit]
+        this.setState({ sources: copySources })
+    }
+  
+    render() {
+        console.log(this.state)
+        return (
+            <div className="bg-near-black white vw-100 vh-100 flex flex-column items-center justify-center">
+                <span className="f2-ns tracked-tight helvetica pa3">cyclepaper</span>
+                {
+                    Object.keys(this.state.sources).map((subReddit) => (
+                        <SubRedditCard selectSubReddit={this.select}>{subReddit}</SubRedditCard>
+                    ))
+                }
+                {/* <button></button> */}
+            </div>
+        );
+    }
 }
 
 export default App;
